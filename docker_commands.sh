f5f4ffd8f5d8a7ab20d8e3b42e6941f6e12ae71c
belwals <saurabh.belwal01@gmail.com>
2022-12-22 07:48:17 +0530

    Added docker docker

---
+++ docker_commands.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+# This file contain common useful docker commands which can be use in the linux system
+# **********************#
+
+# Command return the installed docker's version of docker engine and cli
+docker version
+
+# Info command would gives us more information compare to version
+docker info
+
+
+# Docker follows/support old style command and new style commnds
+# OLD -- docker <command> [option]
+# e.g. docker ps
+# New -- docker <managed command> <sub commands> [option]
+# e.g. docker container ls
+
+
+# Command to run  a image and allowing network routing 
+docker container run <image> [detach, publish port]
+#e.g. `docker container run --publish 8800:80 --detach nginx`
+# OR docker run --name mongo -d mongo , we can give container's name otherwise docker engine will generate a unqique name automatically
+
+# Command to return all container started/Stopped
+docker container ls -a
+
+# Docker to remove container which are not running
+docker container rm <containerIds space separate>
+#e.g. docker container rm 49a2      #<49a2 is just start of the container Id>
+# if in case we want to force terminate, remove running container then
+# e.g. docker container rm -f 49a2
+
+#  Checking the top process on a given container
+docker top <containerId/Name>
+
+
