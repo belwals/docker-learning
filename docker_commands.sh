1574d5e9ffad5896d15a4b63ebbc8bb82873d692
Saurabh Belwal <38013931+belwals@users.noreply.github.com>
2023-12-08 22:59:29 +0530

    Updated document for the docker learning

--- docker_commands.sh
+++ docker_commands.sh
@@ -8,6 +8,8 @@ docker version
 # Info command would gives us more information compare to version
 docker info
 
+# Command to list down all the images
+docker images
 
 # Docker follows/support old style command and new style commnds
 # OLD -- docker <command> [option]
@@ -17,7 +19,7 @@ docker info
 
 
 # Command to run  a image and allowing network routing 
-docker container run <image> [detach, publish port]
+docker container run <image>[:<tag_name>] [detach, publish port]
 #e.g. `docker container run --publish 8800:80 --detach nginx`
 # OR docker run --name mongo -d mongo , we can give container's name otherwise docker engine will generate a unqique name automatically
 
@@ -32,5 +34,11 @@ docker container rm <containerIds space separate>
 
 #  Checking the top process on a given container
 docker top <containerId/Name>
+
+# Getting image from the remote repository
+
+docker pull <Imabge_Name><:[Tag_name]>
+# e.g. docker pull nginx:1.25
+
 
 
