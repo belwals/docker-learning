f71d4d967a7bbacf6e50de677a2647ef921379b7
belwals <saurabh.belwal01@gmail.com>
2024-01-17 08:34:50 +0530

    Fixing the issue of http address on the code and exposing 8080 port from the image | Updated readme with the multi readme referencing

--- golang-app/router/main.go
+++ golang-app/router/main.go
@@ -18,12 +18,13 @@ func main() {
 	// web application's starting point
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "4000"
+		port = "8080"
 	}
+	fmt.Println("---------Starting up the web application---------")
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", welcomePage)
 
-	urlPort := fmt.Sprintf("localhost:%s", port)
+	urlPort := fmt.Sprintf(":%s", port)
 
 	err := http.ListenAndServe(urlPort, mux)
 	if errors.Is(err, http.ErrServerClosed) {
